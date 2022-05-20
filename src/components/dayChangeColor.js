507a86b03e65db167d881547effe4288f29d9ca3
BilalMughal3993 <98820231+BilalMughal3993@users.noreply.github.com>
2022-05-20 17:31:13 +0500

    some changing  and adding a file

---
+++ src/components/dayChangeColor.js
@@ -0,0 +1,15 @@
+import React from 'react'
+const time = "Good Morning"
+const dayChangeColor = () => {
+  return (
+    <>
+    <div className="container">
+        <div className="textS">
+            <h1>Hello Sir, {time}</h1>
+            </div>
+            </div>
+            </>
+  )
+}
+
+export default dayChangeColor
--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 import React from "react";
 import ReactDom from "react-dom";
+import "./index.css"
 
-const curDate = new Date().toLocaleDateString();
-const curTime = new Date().toLocaleTimeString();
+// import dayChangeColor from "./components/dayChangeColor";
+const cssIn = {}
+let greeting="";
+let curDate = new Date();
+curDate=curDate.getHours();
+if(curDate>=5&&curDate<=12)
+{
+  greeting="Good Morning";
+  cssIn.color="red";
+}
+else if(curDate>=12&&curDate<=19)
+{
+greeting="Good Afternoon";
+cssIn.color="green";
+}
+else{
+  greeting="Good Night";
+  cssIn.color="blue"
+}
+// const curTime = new Date().toLocaleTimeString();
+
 ReactDom.render(
  
-  <><h1>Hello World</h1>
+  
+  //  <><h1>Hello World</h1>>
 
 
-  <h1>current date is {curDate}</h1> 
-  <h1>current Time is {curTime}</h1>
-
-  </>,document.getElementById("root"))
+  // <h1>current date is {curDate}</h1> 
+  // <h1>current Time is {curTime}</h1>
+  // </>
+  <>
+  <div className="container">
+  <h1>Hello Sir, <span style={cssIn}>{greeting}</span></h1>
+  </div>
+  </>,
+  
+  document.getElementById("root"))
