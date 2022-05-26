2719c44ccfc8c8a902c1fe9fd74814ce8ac746a6
BilalMughal3993 <98820231+BilalMughal3993@users.noreply.github.com>
2022-05-26 17:55:06 +0500

    some change

--- src/components/SimpleCalculate/SimpleCalculator.js
+++ src/components/SimpleCalculate/SimpleCalculator.js
@@ -5,12 +5,12 @@ const SimpleCalculator = () => {
   return (
     <>
     <div className='container'>
-    <ol className='ok'>
+    <ul className='ok'>
       <li><h1> Adding of 2 and 3 : {add(2,3)}</h1></li>
       <li><h1>subtration of 10 and 3 : {subt(10,3)}</h1></li>
       <li><h1>division of 24 and 3 : {divi(24,3)}</h1></li>
       <li><h1> multiply of 2 and 3 : {multi(2,3)}</h1></li>
-    </ol>
+    </ul>
     </div>
     </>
   )
